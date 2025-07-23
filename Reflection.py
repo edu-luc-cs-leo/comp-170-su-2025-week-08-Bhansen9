9bbcfd4609a3c60d4d04c35c3a7df9b1307cdb1b
Ben Hansen <Benjamin.t.hansen@icloud.com>
2025-07-23 19:26:42 +0000

    ALL DONE

---
+++ Reflection.py
@@ -0,0 +1,11 @@
+"""
+
+After looking at the solutions to week 7 and comparing them to my answers I feel like they are exactly the same. I think I got down the process of making super classes 
+and understand better how the concept works. I think that object oriented programming is amazing and makes programming way more connected and structured rather than having 
+a mess of one file. I think that these past few weeks have taught me the most about how code should be structured and what type of data storage and data manipulation can be 
+used and the syntax of them. I think that I nailed week 7 homework as it is exactly the same as the solutions and I really understood the process that was given to me to learn.
+
+
+
+
+"""
